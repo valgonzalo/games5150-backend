dccd6a99c582852fbc1de741e82cf0962408184b
val.dev <valentinoarellano2006@gmail.com>
2026-06-19 08:24:12 -0300

    Cambios-3

--- src/config/db.js
+++ src/config/db.js
@@ -2,14 +2,8 @@ import { Sequelize } from 'sequelize';
 import { ENVIRONMENT } from './environment.js';
 
 const sequelize = new Sequelize(ENVIRONMENT.DB_URL, {
-  dialect: 'postgres',
+  dialect: 'mysql',
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
 });
 
 export default sequelize;
